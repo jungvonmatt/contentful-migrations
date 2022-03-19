c2752835088eb362327325fee43c3ded04490a55
Ben Zörb <ben@sommerlaune.com>
2022-03-19 01:07:07 +0100

    Adds migration utils (#25)
    * Adds migration utils
    
    * refactor(helpers): refactored helpers
    
    * Removed old comment
    
    * cleanup
    
    * Readme

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+import type Migration, { MigrationContext, MigrationFunction } from "contentful-migration";
+import type { LocaleHelpers } from "./lib/helpers/locale";
+import type { ValidationHelpers } from "./lib/helpers/validation";
+
+export interface MigrationHelpers {
+  locale: LocaleHelpers,
+  validation: ValidationHelpers
+}
+
+export type EnhancedMigrationFunction = (
+  migration: Migration,
+  context?: MigrationContext,
+  helpers?: MigrationHelpers
+) => void;
+
+export function withHelpers(cb: EnhancedMigrationFunction): MigrationFunction;
+export { getLocaleHelpers } from "./lib/helpers/locale";
+export { getValidationHelpers } from "./lib/helpers/validation";
