3d74e97c6040e88a82adc8bccbf0b609655ffffe
Ben Zörb <benjamin.zoerb@jvm.de>
2022-04-26 22:59:50 +0200

    fix: lint + github action

--- index.js
+++ index.js
@@ -11,16 +11,16 @@
  * });
  *
  */
- const { getValidationHelpers } = require('./lib/helpers/validation');
- const { getLocaleHelpers } = require('./lib/helpers/locale');
+const { getValidationHelpers } = require('./lib/helpers/validation');
+const { getLocaleHelpers } = require('./lib/helpers/locale');
 
- // Export wrapper
- module.exports.withHelpers = (cb) => (migration, context) => {
-   const locale = getLocaleHelpers(migration, context);
-   const validation = getValidationHelpers(migration, context);
+// Export wrapper
+module.exports.withHelpers = (cb) => (migration, context) => {
+  const locale = getLocaleHelpers(migration, context);
+  const validation = getValidationHelpers(migration, context);
 
-   return cb(migration, context, { locale, validation });
- };
+  return cb(migration, context, { locale, validation });
+};
 
- module.exports.getValidationHelpers = getValidationHelpers;
- module.exports.getLocaleHelpers = getLocaleHelpers;
+module.exports.getValidationHelpers = getValidationHelpers;
+module.exports.getLocaleHelpers = getLocaleHelpers;
