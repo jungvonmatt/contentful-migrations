9ece4657e10d32fd9b40fb3f4abb8ebf7a7b546b
Thorsten Harders <thorsten.harders@jvm.com>
2025-06-12 13:22:01 +0200

    fix: align naming, tests and readme

--- lib/helpers/validation.d.ts
+++ lib/helpers/validation.d.ts
@@ -1,7 +1,7 @@
 import type Migration from "contentful-migration";
 import type { MigrationContext } from "contentful-migration";
 
-export type ValueMappingFunction = (values: string[]) => string[];
+export type ValueMappingFunction<T extends string = string> = (values: T[]) => T[];
 
 export type AddValuesOptionMode = 'sorted' | 'start' | 'end' | 'before' | 'after';
 
@@ -10,17 +10,20 @@ export interface AddValuesOptions {
   ref?: string;
 }
 
-export type RichTextMarks = 'bold' | 'italic' | 'underline' | 'code';
+// define some known mark and node values here to support code completion, but allow any string as well
+export type RichTextMarks = 'bold' | 'italic' | 'underline' | 'code' | 'superscript' | 'subscript' | 'strikethrough' | string & {};
+export type RichTextNodeType = 'document' | 'paragraph' | 'heading-1' | 'heading-2' | 'heading-3' | 'heading-4' | 'heading-5' | 'heading-6' | 'ordered-list' | 'unordered-list' | 'list-item' | 'hr' | 'blockquote' | 'embedded-entry-block' | 'embedded-asset-block' | 'embedded-resource-block' | 'table' | 'table-row' | 'table-cell' | 'table-header-cell' | 'asset-hyperlink' | 'embedded-entry-inline' | 'embedded-resource-inline' | 'entry-hyperlink' | 'hyperlink' | 'resource-hyperlink' | string & {};
+
 export type RichTextLinkedNodeType = 'entry-hyperlink' | 'embedded-entry-block' | 'embedded-entry-inline'
 
 export interface RichTextValidationHelpers {
-  addEnabledMarksValue(contentTypeId: string, fieldId: string, values: RichTextMarks | RichTextMarks[]): Promise<void>;
-  removeEnabledMarksValue(contentTypeId: string, fieldId: string, values: RichTextMarks | RichTextMarks[]): Promise<void>;
-  modifyEnabledMarksValue(contentTypeId: string, fieldId: string, valueMappingFunction: ValueMappingFunction): Promise<void>;
+  addEnabledMarksValues(contentTypeId: string, fieldId: string, values: RichTextMarks | RichTextMarks[]): Promise<void>;
+  removeEnabledMarksValues(contentTypeId: string, fieldId: string, values: RichTextMarks | RichTextMarks[]): Promise<void>;
+  modifyEnabledMarksValues(contentTypeId: string, fieldId: string, valueMappingFunction: ValueMappingFunction<RichTextMarks>): Promise<void>;
 
-  addEnabledNodeTypeValue(contentTypeId: string, fieldId: string, values: string | string[]): Promise<void>;
-  removeEnabledNodeTypeValue(contentTypeId: string, fieldId: string, values: string | string[]): Promise<void>;
-  modifyEnabledNodeTypeValue(contentTypeId: string, fieldId: string, valueMappingFunction: ValueMappingFunction): Promise<void>;
+  addEnabledNodeTypeValues(contentTypeId: string, fieldId: string, values: RichTextNodeType | RichTextNodeType[]): Promise<void>;
+  removeEnabledNodeTypeValues(contentTypeId: string, fieldId: string, values: RichTextNodeType | RichTextNodeType[]): Promise<void>;
+  modifyEnabledNodeTypeValues(contentTypeId: string, fieldId: string, valueMappingFunction: ValueMappingFunction<RichTextNodeType>): Promise<void>;
 
   addNodeContentTypeValues(contentTypeId: string, fieldId: string, nodeType: RichTextLinkedNodeType, values: string | string[]): Promise<void>;
   removeNodeContentTypeValues(contentTypeId: string, fieldId: string, nodeType: RichTextLinkedNodeType, values: string | string[]): Promise<void>;
