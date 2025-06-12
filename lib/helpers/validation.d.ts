9723440234eee6d99c0cf3f620742262745df9f3
Thorsten Harders <thorsten.harders@jvm.com>
2025-06-12 11:22:54 +0200

    feat: WIP - add richtext validation helper

--- lib/helpers/validation.d.ts
+++ lib/helpers/validation.d.ts
@@ -10,13 +10,33 @@ export interface AddValuesOptions {
   ref?: string;
 }
 
+export type RichTextMarks = 'bold' | 'italic' | 'underline' | 'code';
+export type RichTextLinkedNodeType = 'entry-hyperlink' | 'embedded-entry-block' | 'embedded-entry-inline'
+
+export interface RichTextValidationHelpers {
+  addEnabledMarksValue(contentTypeId: string, fieldId: string, values: RichTextMarks | RichTextMarks[]): Promise<void>;
+  removeEnabledMarksValue(contentTypeId: string, fieldId: string, values: RichTextMarks | RichTextMarks[]): Promise<void>;
+  modifyEnabledMarksValue(contentTypeId: string, fieldId: string, valueMappingFunction: ValueMappingFunction): Promise<void>;
+
+  addEnabledNodeTypeValue(contentTypeId: string, fieldId: string, values: string | string[]): Promise<void>;
+  removeEnabledNodeTypeValue(contentTypeId: string, fieldId: string, values: string | string[]): Promise<void>;
+  modifyEnabledNodeTypeValue(contentTypeId: string, fieldId: string, valueMappingFunction: ValueMappingFunction): Promise<void>;
+
+  addNodeContentTypeValues(contentTypeId: string, fieldId: string, nodeType: RichTextLinkedNodeType, values: string | string[]): Promise<void>;
+  removeNodeContentTypeValues(contentTypeId: string, fieldId: string, nodeType: RichTextLinkedNodeType, values: string | string[]): Promise<void>;
+  modifyNodeContentTypeValues(contentTypeId: string, fieldId: string, nodeType: RichTextLinkedNodeType, valueMappingFunction: ValueMappingFunction): Promise<void>;
+}
+
 export interface ValidationHelpers {
   addLinkContentTypeValues(contentTypeId: string, fieldId: string, values: string | string[]): Promise<void>;
-  addInValues(contentTypeId: string, fieldId: string, values: string | string[], options?: AddValuesOptions): Promise<void>;
   removeLinkContentTypeValues(contentTypeId: string, fieldId: string, values: string | string[]): Promise<void>;
-  removeInValues(contentTypeId: string, fieldId: string, values: string | string[]): Promise<void>;
   modifyLinkContentTypeValues(contentTypeId: string, fieldId: string, valueMappingFunction: ValueMappingFunction): Promise<void>;
+
+  addInValues(contentTypeId: string, fieldId: string, values: string | string[], options?: AddValuesOptions): Promise<void>;
+  removeInValues(contentTypeId: string, fieldId: string, values: string | string[]): Promise<void>;
   modifyInValues(contentTypeId: string, fieldId: string, valueMappingFunction: ValueMappingFunction): Promise<void>;
+
+  richText: RichTextValidationHelpers;
 }
 
 export function getValidationHelpers(migration: Migration, context: MigrationContext): ValidationHelpers;
